7ca9ccf25ceffa27543eaae14f0a706f775aa131
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-05-28 13:03:52 -0300

    Release: Logger sl4j Applied in serviceImp && controller API layer

---
+++ src/main/java/com/project/nuvell/controller/UserEscortController.java
@@ -0,0 +1,57 @@
+package com.project.nuvell.controller;
+
+import com.project.nuvell.entity.UserEscort;
+import com.project.nuvell.service.UserEscortService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/nuvell-api/v1")
+public class UserEscortController {
+
+    private UserEscortService service;
+
+    public UserEscortController(UserEscortService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserEscort>> getAll(){
+        var user = service.findAll();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEscort> getById(@PathVariable Long id){
+        var user = service.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserEscort> post(@RequestBody UserEscort newUser){
+        var user = service.create(newUser);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(user);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserEscort> update(@PathVariable Long id, @RequestBody UserEscort userToUpdate){
+        var user = service.update(id, userToUpdate);
+        return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
--- src/main/java/com/project/nuvell/service/impl/UserEscortServiceImpl.java
+++ src/main/java/com/project/nuvell/service/impl/UserEscortServiceImpl.java
@@ -3,6 +3,8 @@ package com.project.nuvell.service.impl;
 import com.project.nuvell.entity.UserEscort;
 import com.project.nuvell.repository.UserEscortRepository;
 import com.project.nuvell.service.UserEscortService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Service
 public class UserEscortServiceImpl implements UserEscortService {
+
+    private Logger logger = LoggerFactory.getLogger(UserEscortServiceImpl.class.getName());
 
     private final UserEscortRepository userEscortRepository;
 
@@ -20,24 +24,34 @@ public class UserEscortServiceImpl implements UserEscortService {
     @Transactional(readOnly = true)
     @Override
     public List<UserEscort> findAll() {
+        logger.info("Finding a User Escort");
+
         return this.userEscortRepository.findAll();
     }
 
     @Transactional(readOnly = true)
     @Override
     public UserEscort findById(Long id) {
+        logger.info("Finding ALL Users Escorts");
+
         return this.userEscortRepository.findById(id).orElseThrow();
     }
 
     @Transactional
     @Override
     public UserEscort create(UserEscort userEscort) {
+        logger.info("Creating a User Escort");
+
+
         return this.userEscortRepository.save(userEscort);
     }
 
     @Transactional
     @Override
     public UserEscort update(Long id, UserEscort userToUpdate) {
+        logger.info("Updating a User Escort");
+
+
         UserEscort userInDatabase = this.findById(id);
         if (!userInDatabase.getId().equals(userToUpdate.getId())){
             throw new IllegalArgumentException("Update IDs must be the same");
@@ -57,6 +71,8 @@ public class UserEscortServiceImpl implements UserEscortService {
     @Transactional
     @Override
     public void delete(Long id) {
+        logger.info("Deleting a User Escort");
+
         UserEscort userInDB = this.findById(id);
         this.userEscortRepository.delete(userInDB);
     }
