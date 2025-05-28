4607909e06e8a8404e57fb26e0993daa145183c7
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-05-28 08:56:38 -0300

    Project Started
    Signed-off-by: Gabriel Mascarenhas <devgabrielprog@gmail.com>

---
+++ src/main/java/com/project/nuvell/NuvellApplication.java
@@ -0,0 +1,13 @@
+package com.project.nuvell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuvellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/nuvell/NuvellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nuvell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
