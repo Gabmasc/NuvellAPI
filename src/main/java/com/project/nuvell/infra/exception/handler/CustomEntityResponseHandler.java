1e21608a81b3fd67b62f6ec8fc401c2839536e80
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-06-24 10:50:34 -0300

    Release: Implementacao de sobreescrita de metodo da classe ResponseEntityExcetionHandler para tratar error onde o Json nao formata adequadamente o respose da requisacao

--- src/main/java/com/project/nuvell/infra/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/com/project/nuvell/infra/exception/handler/CustomEntityResponseHandler.java
@@ -1,8 +1,11 @@
 package com.project.nuvell.infra.exception.handler;
 
 import com.project.nuvell.infra.exception.*;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -21,6 +24,29 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
                 ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            HttpHeaders headers,
+            HttpStatusCode statusCode,
+            WebRequest request){
+        String cause = ex.getMostSpecificCause().getMessage();
+
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                "Erro no corpo da requisição" + cause,
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<ExceptionResponse> buildResponse(Exception e, WebRequest request, HttpStatus httpStatus) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(InvalidAgeException.class)
